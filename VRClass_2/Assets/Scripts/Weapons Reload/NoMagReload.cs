f184c5ff976ee0a902be27d441d15f20ec3efcab
nezarlawen1 <43337135+nezarlawen1@users.noreply.github.com>
2023-01-17 01:47:40 +0200

    ShotGun Finishing

--- VRClass_2/Assets/Scripts/Weapons Reload/NoMagReload.cs
+++ VRClass_2/Assets/Scripts/Weapons Reload/NoMagReload.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class NoMagReload : MonoBehaviour
 {
-    public int BulletCount;
+    public int BulletCount = 0;
     [SerializeField] List<GameObject> Bullets;
-    private int pointer;
+    //private int pointer;
+    private int i = -1;
 
     private void Start()
     {
-        BulletCount = Bullets.Count;
-        pointer = Bullets.Count - 1;
+        //BulletCount = Bullets.Count;
+        //pointer = Bullets.Count - 1;
+    }
+    public void ActivateBullet()
+    {
+        Bullets[++i].SetActive(true);
+        BulletCount++;
     }
     public void RemoveBullet()
     {
         if (BulletCount >= 0)
         {
-            Bullets[pointer].SetActive(false);
-            pointer--;
+            Bullets[i--].SetActive(false);
+            //pointer--;
             BulletCount--;
+            //i--;
         }
     }
 }
