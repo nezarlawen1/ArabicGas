ed75a4b491429723fa83f66facfea0a6b2627b12
George Rashed <goerge1110@gmail.com>
2023-02-23 02:22:25 +0200

    WaveSystem & Spawners work.

--- VRClass_2/Assets/Scripts/Enemies/Base/WaveSystem.cs
+++ VRClass_2/Assets/Scripts/Enemies/Base/WaveSystem.cs
@@ -9,8 +9,11 @@ public class WaveSystem : MonoBehaviour
     [SerializeField] private int _waveIndex = 1;
     [SerializeField] private int _startEnemyCount = 6;
     [SerializeField] private int _totalEnemyCount = 6;
+    [SerializeField] private int _spawnAtOnceMax = 2;
     [SerializeField] private int _enemySpawnCap = 15;
-    [SerializeField] private int _enemyAmountIndexer = 3;
+    [SerializeField] private int _enemyAmountIndexer = 4;
+
+    [SerializeField] private EnemySpawner[] enemySpawners = new EnemySpawner[0];
 
     private void OnValidate()
     {
@@ -18,10 +21,15 @@ public class WaveSystem : MonoBehaviour
         _totalEnemyCount += _enemyAmountIndexer * (_waveIndex - 1);
     }
 
+    private void Awake()
+    {
+        enemySpawners = GetComponentsInChildren<EnemySpawner>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-
+        InitiateEnemies();
     }
 
     // Update is called once per frame
@@ -30,18 +38,61 @@ public class WaveSystem : MonoBehaviour
 
     }
 
+    private void InitiateEnemies()
+    {
+
+        if (enemySpawners.Length != 0)
+        {
+            int enemiesLeft = _totalEnemyCount;
+            while (enemiesLeft != 0)
+            {
+                for (int i = 0; i < enemySpawners.Length; i++)
+                {
+                    if (enemiesLeft - _spawnAtOnceMax >= 0)
+                    {
+                        enemiesLeft -= _spawnAtOnceMax;
+                        enemySpawners[i].SpawnEnemies(_spawnAtOnceMax);
+                    }
+                    else if (enemiesLeft > 0 && enemiesLeft - _spawnAtOnceMax < 0)
+                    {
+                        enemiesLeft = 0;
+                        enemySpawners[i].SpawnEnemies(Mathf.Abs(enemiesLeft - _spawnAtOnceMax));
+                        return;
+                    }
+                }
+            }
+
+        }
+    }
+
+    private void DestroyAllEnemies()
+    {
+        for (int a = 0; a < enemySpawners.Length; a++)
+        {
+            foreach (var enemyInstance in enemySpawners[a].SpawnedEnemies)
+            {
+                Destroy(enemyInstance);
+            }
+            enemySpawners[a].SpawnedEnemies.Clear();
+        }
+    }
+
     [ContextMenu("Next Round")]
     private void NextRound()
     {
+        DestroyAllEnemies();
         _waveIndex++;
         _totalEnemyCount = _startEnemyCount;
         _totalEnemyCount += _enemyAmountIndexer * (_waveIndex - 1);
+        InitiateEnemies();
     }
 
     [ContextMenu("Reset Rounds")]
     private void ResetRounds()
     {
+        DestroyAllEnemies();
         _waveIndex = 1;
         _totalEnemyCount = _startEnemyCount;
+        InitiateEnemies();
     }
 }
