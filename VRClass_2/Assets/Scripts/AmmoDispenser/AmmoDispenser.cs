3d5af80652af2af7e39ad1b9d1ebbe3f7515b63e
George Rashed <goerge1110@gmail.com>
2023-02-26 05:41:10 +0200

    Set Level, Ammo Dispensing too.

---
+++ VRClass_2/Assets/Scripts/AmmoDispenser/AmmoDispenser.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoDispenser : MonoBehaviour
+{
+    private PointMediator _playerPoints;
+
+    [SerializeField] private Transform _magSpawnLocation;
+    [SerializeField] private int _magsToSpawn = 2, _shellsToSpawn = 6;
+    [SerializeField] private int _price = 500;
+    [SerializeField] private List<GameObject> _itemsList = new List<GameObject>();
+    private GameObject _itemOutcome;
+
+
+    private void Start()
+    {
+        _playerPoints = PointMediator.Instance;
+    }
+
+
+    [ContextMenu("Dispense Ammo")]
+    public void DispenseAmmo()
+    {
+        if (_playerPoints.RemovePoints(_price))
+        {
+
+            int itemIndex = Random.Range(0, _itemsList.Count);
+            _itemOutcome = _itemsList[itemIndex];
+
+            InstantiateItem();
+        }
+    }
+
+    private void InstantiateItem()
+    {
+
+        if (_itemOutcome.TryGetComponent(out Gun gun))
+        {
+            for (int i = 0; i < _magsToSpawn; i++)
+            {
+                gun.CreateMag(_magSpawnLocation);
+            }
+        }
+        else if (_itemOutcome.TryGetComponent(out GunNoMag nomag))
+        {
+            for (int i = 0; i < _shellsToSpawn; i++)
+            {
+                nomag.CreateMag(_magSpawnLocation);
+            }
+        }
+    }
+}
