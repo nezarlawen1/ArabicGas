000bc8122d205ddc06e790f0caa15a8a2b2464d1
George Rashed <goerge1110@gmail.com>
2023-06-15 17:28:36 +0300

    Fixed Generated Texture Quality.

--- ProceduralLandmassGen/Assets/Scripts/TextureGenerator.cs
+++ ProceduralLandmassGen/Assets/Scripts/TextureGenerator.cs
@@ -7,7 +7,7 @@ public static class TextureGenerator
     public static Texture2D TextureFromColorMap(Color[] colorMap, int width, int height)
     {
         Texture2D texture = new Texture2D(width, height);
-        texture.filterMode = FilterMode.Bilinear;
+        texture.filterMode = FilterMode.Point;
         texture.wrapMode = TextureWrapMode.Clamp;
         texture.SetPixels(colorMap);
         texture.Apply();
